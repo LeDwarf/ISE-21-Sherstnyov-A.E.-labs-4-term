c7f7d97f5251ae45dcb681ea485d1b6fad575e1d
Alexey <sherstalex@yandex.ru>
2018-06-08 17:33:18 +0400

    lab 15 desktop

--- AlexeysShopService/ViewModels/CustomerViewModel.cs
+++ AlexeysShopService/ViewModels/CustomerViewModel.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace AlexeysShopService.ViewModels
 {
+    [DataContract]
     public class CustomerViewModel
     {
+        [DataMember]
         public int Id { get; set; }
+        [DataMember]
+        public string Mail { get; set; }
+        [DataMember]
         public string CustomerFIO { get; set; }
+        [DataMember]
+        public List<MessageInfoViewModel> Messages { get; set; }
+
     }
 }
--- AlexeysShopService/ViewModels/StorageViewModel.cs
+++ AlexeysShopService/ViewModels/StorageViewModel.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace AlexeysShopService.ViewModels
 {
+    [DataContract]
     public class StorageViewModel
     {
+        [DataMember]
         public int Id { get; set; }
+        [DataMember]
         public string StorageName { get; set; }
+        [DataMember]
         public List<StoragePartViewModel> StorageParts { get; set; }
     }
 }
