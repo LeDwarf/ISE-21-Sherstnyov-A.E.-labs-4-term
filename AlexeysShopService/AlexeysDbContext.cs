37d838ed75c3d5e8e1d04bd1a10343995dca5160
Alexey <sherstalex@yandex.ru>
2018-06-08 17:29:57 +0400

    lab 14 desktop

--- AlexeysShopService/AlexeysDbContext.cs
+++ AlexeysShopService/AlexeysDbContext.cs
@@ -1,33 +1,62 @@
 ﻿using AlexeysShopModel;
-using System.ComponentModel.DataAnnotations.Schema;
+using System;
 using System.Data.Entity;
 
 namespace AlexeysShopService
 {
-	[Table("AlexeysDatabase")]
-	public class AlexeysDbContext : DbContext
-	{
-		public AlexeysDbContext()
-		{
-			Configuration.ProxyCreationEnabled = false;
-			Configuration.LazyLoadingEnabled = false;
-			var ensureDLLIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
-		}
+    public class AlexeysDbContext : DbContext
+    {
+        public AlexeysDbContext() : base("AlexeysDatabase")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+            var ensureDLLIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
 
-		public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
 
-		public virtual DbSet<Part> Parts { get; set; }
+        public virtual DbSet<Part> Parts { get; set; }
 
-		public virtual DbSet<Builder> Builders { get; set; }
+        public virtual DbSet<Builder> Builders { get; set; }
 
-		public virtual DbSet<Contract> Contracts { get; set; }
+        public virtual DbSet<Contract> Contracts { get; set; }
 
-		public virtual DbSet<Article> Articles { get; set; }
+        public virtual DbSet<Article> Articles { get; set; }
 
-		public virtual DbSet<ArticlePart> ArticleParts { get; set; }
+        public virtual DbSet<ArticlePart> ArticleParts { get; set; }
 
-		public virtual DbSet<Storage> Storages { get; set; }
+        public virtual DbSet<Storage> Storages { get; set; }
 
-		public virtual DbSet<StoragePart> StorageParts { get; set; }
-	}
+        public virtual DbSet<StoragePart> StorageParts { get; set; }
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (Exception)
+            {
+                foreach (var entry in ChangeTracker.Entries())
+                {
+                    switch (entry.State)
+                    {
+                        case EntityState.Modified:
+                            entry.State = EntityState.Unchanged;
+                            break;
+                        case EntityState.Deleted:
+                            entry.Reload();
+                            break;
+                        case EntityState.Added:
+                            entry.State = EntityState.Detached;
+                            break;
+                    }
+                }
+                throw;
+            }
+        }
+    }
 }
--- AlexeysShopView/FormTakeContractInWork.cs
+++ AlexeysShopView/FormTakeContractInWork.cs
@@ -2,88 +2,93 @@
 using AlexeysShopService.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AlexeysShopView
 {
-	public partial class FormTakeContractInWork : Form
-	{
-		public int Id { set { id = value; } }
+    public partial class FormTakeContractInWork : Form
+    {
+        public int Id { set { id = value; } }
 
-		private int? id;
+        private int? id;
 
-		public FormTakeContractInWork()
-		{
-			InitializeComponent();
-		}
+        public FormTakeContractInWork()
+        {
+            InitializeComponent();
+        }
 
-		private void FormTakeContractInWork_Load(object sender, EventArgs e)
-		{
-			try
-			{
-				if (!id.HasValue)
-				{
-					MessageBox.Show("Не указан заказ", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-					Close();
-				}
-				var response = APIClient.GetRequest("api/Builder/GetList");
-				if (response.Result.IsSuccessStatusCode)
-				{
-					List<BuilderViewModel> list = APIClient.GetElement<List<BuilderViewModel>>(response);
-					if (list != null)
-					{
-						comboBoxBuilder.DisplayMember = "BuilderFIO";
-						comboBoxBuilder.ValueMember = "Id";
-						comboBoxBuilder.DataSource = list;
-						comboBoxBuilder.SelectedItem = null;
-					}
-				}
-				else
-				{
-					throw new Exception(APIClient.GetError(response));
-				}
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-			}
-		}
+        private void FormTakeContractInWork_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!id.HasValue)
+                {
+                    MessageBox.Show("Не указан заказ", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Close();
+                }
+                List<BuilderViewModel> list = Task.Run(() => APIClient.GetRequestData<List<BuilderViewModel>>("api/Builder/GetList")).Result;
+                if (list != null)
+                {
+                    comboBoxBuilder.DisplayMember = "BuilderFIO";
+                    comboBoxBuilder.ValueMember = "Id";
+                    comboBoxBuilder.DataSource = list;
+                    comboBoxBuilder.SelectedItem = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                }
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
-		private void buttonSave_Click(object sender, EventArgs e)
-		{
-			if (comboBoxBuilder.SelectedValue == null)
-			{
-				MessageBox.Show("Выберите исполнителя", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
-			try
-			{
-				var response = APIClient.PostRequest("api/General/TakeContractInWork", new ContractBindingModel
-				{
-					Id = id.Value,
-					BuilderId = Convert.ToInt32(comboBoxBuilder.SelectedValue)
-				});
-				if (response.Result.IsSuccessStatusCode)
-				{
-					MessageBox.Show("Сохранение прошло успешно", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
-					DialogResult = DialogResult.OK;
-					Close();
-				}
-				else
-				{
-					throw new Exception(APIClient.GetError(response));
-				}
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-			}
-		}
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (comboBoxBuilder.SelectedValue == null)
+            {
+                MessageBox.Show("Выберите исполнителя", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                int BuilderId = Convert.ToInt32(comboBoxBuilder.SelectedValue);
+                Task task = Task.Run(() => APIClient.PostRequestData("api/General/TakeContractInWork", new ContractBindingModel
+                {
+                    Id = id.Value,
+                    BuilderId = BuilderId
+                }));
 
-		private void buttonCancel_Click(object sender, EventArgs e)
-		{
-			DialogResult = DialogResult.Cancel;
-			Close();
-		}
-	}
+                task.ContinueWith((prevTask) => MessageBox.Show("Заказ передан в работу. Обновите список", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information),
+                    TaskContinuationOptions.OnlyOnRanToCompletion);
+                task.ContinueWith((prevTask) =>
+                {
+                    var ex = (Exception)prevTask.Exception;
+                    while (ex.InnerException != null)
+                    {
+                        ex = ex.InnerException;
+                    }
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }, TaskContinuationOptions.OnlyOnFaulted);
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                }
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
 }
