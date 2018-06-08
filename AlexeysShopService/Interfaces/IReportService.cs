509385858a8af49c04972f54eba22f6494d0909b
Alexey <sherstalex@yandex.ru>
2018-06-08 17:07:29 +0400

    lab 12 desktop

---
+++ AlexeysShopService/Interfaces/IReportService.cs
@@ -0,0 +1,19 @@
+﻿using AlexeysShopService.BindingModels;
+using AlexeysShopService.ViewModels;
+using System.Collections.Generic;
+
+namespace AlexeysShopService.Interfaces
+{
+    public interface IReportService
+    {
+        void SaveArticlePrice(ReportBindingModel model);
+
+        List<StoragesLoadViewModel> GetStoragesLoad();
+
+        void SaveStoragesLoad(ReportBindingModel model);
+
+        List<CustomerContractsModel> GetCustomerContracts(ReportBindingModel model);
+
+        void SaveCustomerContracts(ReportBindingModel model);
+    }
+}
---
+++ AlexeysShopService/ViewModels/CustomerContractsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AlexeysShopService.ViewModels
+{
+    public class CustomerContractsModel
+    {
+        public string CustomerName { get; set; }
+
+        public string DateCreate { get; set; }
+
+        public string ArticleName { get; set; }
+
+        public int Count { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public string Status { get; set; }
+    }
+}
