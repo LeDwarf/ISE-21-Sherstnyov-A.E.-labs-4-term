87948eddbec6ab35b2cc29cef26eb832fae49c65
Alexey <sherstalex@yandex.ru>
2018-06-09 09:56:57 +0400

    laba9

---
+++ AlexeysShopService/ViewModels/StorageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexeysShopService.ViewModels
+{
+    public class StorageViewModel
+    {
+        public int Id { get; set; }
+        public string StorageName { get; set; }
+        public List<StoragePartViewModel> StorageParts { get; set; }
+    }
+}
