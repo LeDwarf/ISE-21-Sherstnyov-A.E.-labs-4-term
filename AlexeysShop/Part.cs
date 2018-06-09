bf30d9fa3607b27bb40de32ce02121c70eeff77e
Alexey <sherstalex@yandex.ru>
2018-06-09 10:02:30 +0400

    laba11

--- AlexeysShop/Part.cs
+++ AlexeysShop/Part.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AlexeysShopModel
 {
     public class Part
     {
         public int Id { get; set; }
-        public string PartName { get; set; }
-    }
+
+		[Required]
+		public string PartName { get; set; }
+
+		[ForeignKey("PartId")]
+		public virtual List<ArticlePart> ArticleParts { get; set; }
+
+		[ForeignKey("PartId")]
+		public virtual List<StoragePart> StorageParts { get; set; }
+	}
 }
